ab3f6455d0b94bc9b292cc185997e162facd35d0
Martincz Gao <martincz@mokeedev.com>
2022-07-12 18:46:57 +0800

    lake: mokee-ify
    Change-Id: Ice73f7d1b9f190add5f955a39ace11761c28e89e

--- mokee_lake.mk
+++ mokee_lake.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 #
 
-# Inherit some common Lineage stuff.
+# Inherit some common MoKee stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/mokee/config/common_full_phone.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
@@ -54,7 +54,7 @@ TARGET_RECOVERY_FSTAB := device/motorola/sdm660-common/rootdir/etc/fstab.qcom
 
 # Device identifiers
 PRODUCT_DEVICE := lake
-PRODUCT_NAME := lineage_lake
+PRODUCT_NAME := mokee_lake
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g(7) plus
 PRODUCT_MANUFACTURER := motorola
